97c106ee93618d96deaa2c9c70ce5dccb77ae5ed
Fhel <159400656+Pilo067@users.noreply.github.com>
2026-02-04 18:32:03 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+// Create a new list item when clicking on the &quot;Add&quot; button
+function addListItem() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("taskInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        document.getElementById("taskList").appendChild(li);
+    }
+    document.getElementById("taskInput").value = "";
+}
